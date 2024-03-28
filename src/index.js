2e14b624fb0a1b45ff88771748826e79399bfd63
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-03-28 11:46:01 -0700

    adding some html to the project and css for style

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './css/styles.css';
 // first i have to get the api of the weather
+
+
+
+fetch(`https://api.weatherapi.com/v1/current.json?key=2bb56963b22d402aaab180509242803&q=london`)
+.then(response=>response.json())
+.then(data=>console.log(data));
