b66727892bdae642e0b2f470caeba6989a60666d
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-03-29 12:15:59 -0700

    trying some stuff no JS related with the weather app. I copied some aspects of the html and css from a tutorial just to be able to start seeing how weather apps look like and to have the data il get somewhere

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import './css/styles.css';
 
 
 
-fetch(`https://api.weatherapi.com/v1/current.json?key=2bb56963b22d402aaab180509242803&q=london`)
-.then(response=>response.json())
-.then(data=>console.log(data));
+// fetch(`https://api.weatherapi.com/v1/current.json?key=2bb56963b22d402aaab180509242803&q=london`)
+// .then(response=>response.json())
+// .then(data=>console.log(data));
+
+
+const apiKey= `2bb56963b22d402aaab180509242803`;
+const apiUrl=`https://api.weatherapi.com/v1/current.json?key=`;
+const apiUrl2 = `&q=`
+let location = 'london'
+
+console.log(apiUrl + apiKey + apiUrl2 + location)
+
+// async function checkWeather(){
+//     const response = await fetch(apiUrl + apiKey + apiUrl2 + location);
+//     var data = await response.json();
+
+//     console.log(data);
+// }
